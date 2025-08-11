6354ef7e3ad3f1acc9485d10eb4321b94f745611
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2025-08-11 23:45:42 +0300

    feat: add base layout

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const gameWrapper = document.querySelector(".game");
+const registerWrapper = document.querySelector(".register");
 const homeWrapper = document.querySelector(".home");
 const characterWrapper = document.querySelector(".character");
 const settingsWrapper = document.querySelector(".settings");
@@ -6,12 +7,16 @@ const homeButton = document.querySelector(".home-button");
 const gameButton = document.querySelector(".game-button");
 const characterButton = document.querySelector(".character-button");
 const settingsButton = document.querySelector(".settings-button");
+const registerButton = document.querySelector(".register-button");
+const pageTitle = document.querySelector(".header__page-title");
 
 const home = () => {
   gameWrapper.style.display = "none";
   homeWrapper.style.display = "block";
   characterWrapper.style.display = "none";
   settingsWrapper.style.display = "none";
+  registerWrapper.style.display = "none";
+  pageTitle.textContent = "Home";
 };
 
 const game = () => {
@@ -19,13 +24,17 @@ const game = () => {
   homeWrapper.style.display = "none";
   characterWrapper.style.display = "none";
   settingsWrapper.style.display = "none";
+  registerWrapper.style.display = "none";
+  pageTitle.textContent = "Battle";
 };
 
 const character = () => {
   gameWrapper.style.display = "none";
   homeWrapper.style.display = "none";
-  characterWrapper.style.display = "block";
+  characterWrapper.style.display = "flex";
   settingsWrapper.style.display = "none";
+  registerWrapper.style.display = "none";
+  pageTitle.textContent = "Character";
 };
 
 const settings = () => {
@@ -33,9 +42,12 @@ const settings = () => {
   homeWrapper.style.display = "none";
   characterWrapper.style.display = "none";
   settingsWrapper.style.display = "block";
+  registerWrapper.style.display = "none";
+  pageTitle.textContent = "Settings";
 };
 
 homeButton.addEventListener("click", home);
 gameButton.addEventListener("click", game);
 characterButton.addEventListener("click", character);
 settingsButton.addEventListener("click", settings);
+registerButton.addEventListener("click", home);

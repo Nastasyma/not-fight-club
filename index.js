0d4607d35114fd9b4e5dd89ca3919c41d6c0e76e
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2025-08-13 00:51:40 +0300

    refactor: edit game layout, add name to LS

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const screens = document.querySelectorAll('.screen');
 const buttons = document.querySelectorAll('.button');
-const settingsName = document.querySelector('.settings__name-wrapper');
-const settingsInput = document.querySelector('.settings__input-wrapper');
+const settingsNameWrapper = document.querySelector('.settings__name-wrapper');
+const settingsInputWrapper = document.querySelector('.settings__input-wrapper');
 const settingsSave = document.querySelector('.settings__save-button');
 const settingsChange = document.querySelector('.settings__change-button');
 const pageTitle = document.querySelector('.header__page-title');
+const characterPopup = document.querySelector('.character__popup');
+const characterEdit = document.querySelector('.character__edit');
+const characterPopupClose = document.querySelector('.character__popup-close');
+const registerButton = document.querySelector('.register-button');
+const nameInput = document.getElementById('name');
+const characterName = document.querySelector('.character__name');
+const gameCharacterName = document.querySelector('.game__character-name');
+const settingsName = document.querySelector('.settings__name');
+const settingsInput = document.getElementById('settings-name');
+const popupImages = document.querySelectorAll('.character__popup-item img');
+const characterImage = document.querySelector('.character__image');
+const gamePlayerImage = document.querySelector('.player-avatar img');
 
 function showScreen(screenName) {
   screens.forEach((screen) => {
@@ -22,67 +34,67 @@ buttons.forEach((button) => {
 });
 
 settingsChange.addEventListener('click', () => {
-  settingsName.style.display = 'none';
-  settingsInput.style.display = 'flex';
+  settingsNameWrapper.style.display = 'none';
+  settingsInputWrapper.style.display = 'flex';
   settingsSave.style.display = 'flex';
 });
 
 settingsSave.addEventListener('click', () => {
-  settingsName.style.display = 'flex';
-  settingsInput.style.display = 'none';
+  settingsNameWrapper.style.display = 'flex';
+  settingsInputWrapper.style.display = 'none';
   settingsSave.style.display = 'none';
 });
 
-// const gameWrapper = document.querySelector(".game");
-// const registerWrapper = document.querySelector(".register");
-// const homeWrapper = document.querySelector(".home");
-// const characterWrapper = document.querySelector(".character");
-// const settingsWrapper = document.querySelector(".settings");
-// const homeButton = document.querySelector(".home-button");
-// const gameButton = document.querySelector(".game-button");
-// const characterButton = document.querySelector(".character-button");
-// const settingsButton = document.querySelector(".settings-button");
-// const registerButton = document.querySelector(".register-button");
-// const pageTitle = document.querySelector(".header__page-title");
+characterEdit.addEventListener('click', () => {
+  characterPopup.classList.add('active');
+});
 
-// const home = () => {
-//   gameWrapper.style.display = "none";
-//   homeWrapper.style.display = "block";
-//   characterWrapper.style.display = "none";
-//   settingsWrapper.style.display = "none";
-//   registerWrapper.style.display = "none";
-//   pageTitle.textContent = "Home";
-// };
+characterPopupClose.addEventListener('click', () => {
+  characterPopup.classList.remove('active');
+});
 
-// const game = () => {
-//   gameWrapper.style.display = "block";
-//   homeWrapper.style.display = "none";
-//   characterWrapper.style.display = "none";
-//   settingsWrapper.style.display = "none";
-//   registerWrapper.style.display = "none";
-//   pageTitle.textContent = "Battle";
-// };
+popupImages.forEach((image) => {
+  image.addEventListener('click', () => {
+    characterImage.src = image.src;
+    gamePlayerImage.src = image.src;
+    characterPopup.classList.remove('active');
+  });
+});
 
-// const character = () => {
-//   gameWrapper.style.display = "none";
-//   homeWrapper.style.display = "none";
-//   characterWrapper.style.display = "flex";
-//   settingsWrapper.style.display = "none";
-//   registerWrapper.style.display = "none";
-//   pageTitle.textContent = "Character";
-// };
+registerButton.addEventListener('click', () => {
+  localStorage.setItem('_NFC-name', nameInput.value);
+  setCharacterName();
+});
 
-// const settings = () => {
-//   gameWrapper.style.display = "none";
-//   homeWrapper.style.display = "none";
-//   characterWrapper.style.display = "none";
-//   settingsWrapper.style.display = "block";
-//   registerWrapper.style.display = "none";
-//   pageTitle.textContent = "Settings";
-// };
+settingsSave.addEventListener('click', () => {
+  localStorage.setItem('_NFC-name', settingsInput.value);
+  setCharacterName();
+});
 
-// homeButton.addEventListener("click", home);
-// gameButton.addEventListener("click", game);
-// characterButton.addEventListener("click", character);
-// settingsButton.addEventListener("click", settings);
-// registerButton.addEventListener("click", home);
+nameInput.addEventListener('input', () => {
+  if (nameInput.value.length >= 1) {
+    registerButton.disabled = false;
+  } else {
+    registerButton.disabled = true;
+  }
+});
+
+settingsInput.addEventListener('input', () => {
+  if (settingsInput.value.length >= 1) {
+    settingsSave.disabled = false;
+  } else {
+    settingsSave.disabled = true;
+  }
+});
+
+function setCharacterName() {
+  if (!localStorage.getItem('_NFC-name')) {
+    return;
+  }
+  characterName.textContent = localStorage.getItem('_NFC-name');
+  gameCharacterName.textContent = localStorage.getItem('_NFC-name');
+  settingsName.textContent = localStorage.getItem('_NFC-name');
+}
+
+showScreen('register');
+setCharacterName();

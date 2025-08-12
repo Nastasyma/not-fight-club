64154715b98616c6c6821fd2fa28c6f2ae47db4a
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2025-08-12 07:48:17 +0300

    feat: add prettier

--- index.js
+++ index.js
@@ -1,53 +1,70 @@
-const gameWrapper = document.querySelector(".game");
-const registerWrapper = document.querySelector(".register");
-const homeWrapper = document.querySelector(".home");
-const characterWrapper = document.querySelector(".character");
-const settingsWrapper = document.querySelector(".settings");
-const homeButton = document.querySelector(".home-button");
-const gameButton = document.querySelector(".game-button");
-const characterButton = document.querySelector(".character-button");
-const settingsButton = document.querySelector(".settings-button");
-const registerButton = document.querySelector(".register-button");
-const pageTitle = document.querySelector(".header__page-title");
+// const gameWrapper = document.querySelector(".game");
+// const registerWrapper = document.querySelector(".register");
+// const homeWrapper = document.querySelector(".home");
+// const characterWrapper = document.querySelector(".character");
+// const settingsWrapper = document.querySelector(".settings");
+// const homeButton = document.querySelector(".home-button");
+// const gameButton = document.querySelector(".game-button");
+// const characterButton = document.querySelector(".character-button");
+// const settingsButton = document.querySelector(".settings-button");
+// const registerButton = document.querySelector(".register-button");
+// const pageTitle = document.querySelector(".header__page-title");
 
-const home = () => {
-  gameWrapper.style.display = "none";
-  homeWrapper.style.display = "block";
-  characterWrapper.style.display = "none";
-  settingsWrapper.style.display = "none";
-  registerWrapper.style.display = "none";
-  pageTitle.textContent = "Home";
-};
+// const home = () => {
+//   gameWrapper.style.display = "none";
+//   homeWrapper.style.display = "block";
+//   characterWrapper.style.display = "none";
+//   settingsWrapper.style.display = "none";
+//   registerWrapper.style.display = "none";
+//   pageTitle.textContent = "Home";
+// };
 
-const game = () => {
-  gameWrapper.style.display = "block";
-  homeWrapper.style.display = "none";
-  characterWrapper.style.display = "none";
-  settingsWrapper.style.display = "none";
-  registerWrapper.style.display = "none";
-  pageTitle.textContent = "Battle";
-};
+// const game = () => {
+//   gameWrapper.style.display = "block";
+//   homeWrapper.style.display = "none";
+//   characterWrapper.style.display = "none";
+//   settingsWrapper.style.display = "none";
+//   registerWrapper.style.display = "none";
+//   pageTitle.textContent = "Battle";
+// };
 
-const character = () => {
-  gameWrapper.style.display = "none";
-  homeWrapper.style.display = "none";
-  characterWrapper.style.display = "flex";
-  settingsWrapper.style.display = "none";
-  registerWrapper.style.display = "none";
-  pageTitle.textContent = "Character";
-};
+// const character = () => {
+//   gameWrapper.style.display = "none";
+//   homeWrapper.style.display = "none";
+//   characterWrapper.style.display = "flex";
+//   settingsWrapper.style.display = "none";
+//   registerWrapper.style.display = "none";
+//   pageTitle.textContent = "Character";
+// };
 
-const settings = () => {
-  gameWrapper.style.display = "none";
-  homeWrapper.style.display = "none";
-  characterWrapper.style.display = "none";
-  settingsWrapper.style.display = "block";
-  registerWrapper.style.display = "none";
-  pageTitle.textContent = "Settings";
-};
+// const settings = () => {
+//   gameWrapper.style.display = "none";
+//   homeWrapper.style.display = "none";
+//   characterWrapper.style.display = "none";
+//   settingsWrapper.style.display = "block";
+//   registerWrapper.style.display = "none";
+//   pageTitle.textContent = "Settings";
+// };
 
-homeButton.addEventListener("click", home);
-gameButton.addEventListener("click", game);
-characterButton.addEventListener("click", character);
-settingsButton.addEventListener("click", settings);
-registerButton.addEventListener("click", home);
+// homeButton.addEventListener("click", home);
+// gameButton.addEventListener("click", game);
+// characterButton.addEventListener("click", character);
+// settingsButton.addEventListener("click", settings);
+// registerButton.addEventListener("click", home);
+
+const screens = document.querySelectorAll('.screen');
+const buttons = document.querySelectorAll('.button');
+
+function showScreen(screenName) {
+  screens.forEach((screen) => {
+    screen.classList.remove('active');
+  });
+  document.querySelector(`.${screenName}`).classList.add('active');
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const screenName = button.getAttribute('data-screen');
+    showScreen(screenName);
+  });
+});
